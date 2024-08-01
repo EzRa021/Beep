04d6a350616e32d67c1b7cceb13ff561440f4692
Olamide <badejoezra0@gmail.com>
2024-08-01 16:30:39 +0100

    Houdini

--- src/pages/marketPlace.jsx
+++ src/pages/marketPlace.jsx
@@ -26,10 +26,6 @@ const MarketPlace = () => {
         return <div className=' w-full h-[400px] text-center text-4xl font-bold '>{error}</div>;
     }
 
-    if (!filteredAds.length) {
-        return <div>No ad found</div>;
-    }
-
     return (
         <div>
             {/* <!-- recent-post section start  --> */}
@@ -58,56 +54,62 @@ const MarketPlace = () => {
                             <option value="Others">Others</option>
                         </select>
                     </div>
-                    <div className="row">
-                        {filteredAds.map((ad) => (
-                            <div className="col-xl-3 col-md-6" key={ad._id}>
-                                <div className="cards cards--one">
-                                    <a href={`/single-ad/${ad._id}`} className="cards__img-wrapper">
-                                        <img src={ad.images[0]} alt="card-img" className="img-fluid" />
-                                    </a>
-                                    <div className="cards__info">
-                                        <div className="cards__info-top">
-                                            <h6 className="text--body-4 cards__category-title">
-                                                <span className="icon">
-                                                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M2 11L8 14.5L14 11" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                        <path d="M2 8L8 11.5L14 8" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                        <path d="M2 5L8 8.5L14 5L8 1.5L2 5Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
-                                                    </svg>
-                                                </span>
-                                                {ad.adName}
-                                            </h6>
-                                            <a href={`/single-ad/${ad._id}`} className="text--body-3-600 cards__caption-title line-clamp-2"><b>{ad.description}</b></a>
-                                        </div>
-                                        <div className="cards__info-bottom">
-                                            <h6 className="cards__location text--body-4">
-                                                <span className="icon">
-                                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path
-                                                            d="M10 10.625C11.3807 10.625 12.5 9.50571 12.5 8.125C12.5 6.74429 11.3807 5.625 10 5.625C8.61929 5.625 7.5 6.74429 7.5 8.125C7.5 9.50571 8.61929 10.625 10 10.625Z"
-                                                            stroke="#27C200"
-                                                            strokeWidth="1.2"
-                                                            strokeLinecap="round"
-                                                            strokeLinejoin="round"
-                                                        ></path>
-                                                        <path
-                                                            d="M16.25 8.125C16.25 13.75 10 18.125 10 18.125C10 18.125 3.75 13.75 3.75 8.125C3.75 6.4674 4.40848 4.87769 5.58058 3.70558C6.75269 2.53348 8.3424 1.875 10 1.875C11.6576 1.875 13.2473 2.53348 14.4194 3.70558C15.5915 4.87769 16.25 6.4674 16.25 8.125V8.125Z"
-                                                            stroke="#27C200"
-                                                            strokeWidth="1.2"
-                                                            strokeLinecap="round"
-                                                            strokeLinejoin="round"
-                                                        ></path>
-                                                    </svg>
-                                                </span>
-                                                {ad.location}
-                                            </h6>
-                                            <span className="cards__price-title text--body-3-600">{ad.price}</span>
+                    {!filteredAds.length ?
+                        <div className=' flex justify-center items-center h-[400px]'><h1 className=' text-4xl font-semibold'> No ad found :)</h1></div> :
+                        <div className="row">
+                            {filteredAds.map((ad) => (
+                                <div className="col-xl-3 col-md-6" key={ad._id}>
+                                    <div className="cards cards--one">
+                                        <a href={`/single-ad/${ad._id}`} className="cards__img-wrapper">
+                                            <img src={ad.images[0]} alt="card-img" className="img-fluid" />
+                                        </a>
+                                        <div className="cards__info">
+                                            <div className="cards__info-top">
+                                                <h6 className="text--body-4 cards__category-title">
+                                                    <span className="icon">
+                                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                            <path d="M2 11L8 14.5L14 11" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                            <path d="M2 8L8 11.5L14 8" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                            <path d="M2 5L8 8.5L14 5L8 1.5L2 5Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"></path>
+                                                        </svg>
+                                                    </span>
+                                                    {ad.adName}
+                                                </h6>
+                                                <a href={`/single-ad/${ad._id}`} className="text--body-3-600 cards__caption-title line-clamp-2"><b>{ad.description}</b></a>
+                                            </div>
+                                            <div className="cards__info-bottom">
+                                                <h6 className="cards__location text--body-4">
+                                                    <span className="icon">
+                                                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                            <path
+                                                                d="M10 10.625C11.3807 10.625 12.5 9.50571 12.5 8.125C12.5 6.74429 11.3807 5.625 10 5.625C8.61929 5.625 7.5 6.74429 7.5 8.125C7.5 9.50571 8.61929 10.625 10 10.625Z"
+                                                                stroke="#27C200"
+                                                                strokeWidth="1.2"
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                            ></path>
+                                                            <path
+                                                                d="M16.25 8.125C16.25 13.75 10 18.125 10 18.125C10 18.125 3.75 13.75 3.75 8.125C3.75 6.4674 4.40848 4.87769 5.58058 3.70558C6.75269 2.53348 8.3424 1.875 10 1.875C11.6576 1.875 13.2473 2.53348 14.4194 3.70558C15.5915 4.87769 16.25 6.4674 16.25 8.125V8.125Z"
+                                                                stroke="#27C200"
+                                                                strokeWidth="1.2"
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                            ></path>
+                                                        </svg>
+                                                    </span>
+                                                    {ad.location}
+                                                </h6>
+                                                <span className="cards__price-title text--body-3-600">{ad.price}</span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
-                    </div>
+                            ))}
+                        </div>
+                    }
+
+
+
                 </div>
             </section>
             {/* <!-- recent-post section end --> */}
